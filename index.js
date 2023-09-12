e94d4117a2d1641ea529d3e54cddc8aedb9acffc
Elguja Bekoshvili <g.bekoshvili@ncdc.local>
2023-09-12 15:13:10 +0400

    transition

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const register = document.getElementById('account')
 const rContainer = document.getElementById('rContainer')
+const login = document.getElementById('re-account')
+const lContainer = document.getElementById('lContainer')
 
 register.addEventListener('click', () => {
     rContainer.style.display = 'flex'
-    document.getElementById('lContainer').style.display = 'none'
+    lContainer.style.display = 'none'
 
     setTimeout(() => {
         rContainer.classList.add('registerAnim')
+        lContainer.classList.remove('loginAnim')
+    }, 100);
+})
+
+login.addEventListener('click', () => {
+    lContainer.style.display = 'flex'
+    rContainer.style.display = 'none'
+
+    setTimeout(() => {
+        lContainer.classList.add('loginAnim')
+        rContainer.classList.remove('registerAnim')
     }, 100);
 })
